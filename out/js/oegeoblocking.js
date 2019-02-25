a79e56d774fdad189c95de2786c7c6cd0c0df2bb
Mantas Vaitkunas <manvait@gmail.com>
2019-02-25 15:27:49 +0200

    OXDEV-1771 Cleanup JS file

--- out/js/oegeoblocking.js
+++ out/js/oegeoblocking.js
@@ -4,7 +4,6 @@
  */
 var oeGeoBlocking = {
     isCountryInvoiceOnly: 0,
-    selectedCountryId: false,
 
     /**
      * Initializes after document.ready.
@@ -14,10 +13,6 @@ var oeGeoBlocking = {
             // event listener: selected invoice country has changed
             $(document.body).off('change', "#invCountrySelect", oeGeoBlocking.checkInvoiceCountry);
             $(document.body).on('change', "#invCountrySelect", oeGeoBlocking.checkInvoiceCountry);
-
-            // event listener: edit invoice address has been clicked
-            $('#userChangeAddress').off('click', oeGeoBlocking.checkInvoiceCountry);
-            $('#userChangeAddress').on('click', oeGeoBlocking.checkInvoiceCountry);
 
             // initial check
             oeGeoBlocking.checkInvoiceCountry();
@@ -31,21 +26,16 @@ var oeGeoBlocking = {
         oeGeoBlocking.isCountryInvoiceOnly = $("#invCountrySelect").find(':selected').data('invoiceonly');
 
         if (oeGeoBlocking.isCountryInvoiceOnly == 1) {
-            // country is marked as "invoice only"
+            // Country is marked as "invoice only"
             if (!$('#shippingAddress').is(':visible')) {
                 $('#showShipAddress').click();
             }
             $('.invoiceonlyhint').removeClass('hidden');
             $('#showShipAddress').attr("disabled", "disabled");
         } else {
-            // country is NOT marked as "invoice only"
-            if ($('#shippingAddress').is(':visible')) {
-                $('#showShipAddress').removeAttr("disabled");
-                $('.invoiceonlyhint').addClass('hidden');
-            } else {
-                $('#showShipAddress').removeAttr("disabled");
-                $('.invoiceonlyhint').addClass('hidden');
-            }
+            // Country is NOT marked as "invoice only"
+            $('.invoiceonlyhint').addClass('hidden');
+            $('#showShipAddress').removeAttr("disabled");
         }
     },
 };
