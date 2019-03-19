2256834dc6778437322aded567e8b12e4ef0d113
Juergen-Busch <juergen.busch@oxid-esales.com>
2019-03-19 15:04:00 +0100

    Fixed typo

--- Application/views/admin/de/oegeoblocking_lang.php
+++ Application/views/admin/de/oegeoblocking_lang.php
@@ -5,7 +5,7 @@ $sLangName = 'Deutsch';
 $aLang = array(
     'charset'                 => 'UTF-8',
 
-    'OEGEOBLOCKING_INVOICEONLY'             => 'Land gehört nicht zum Liefergebiet"',
+    'OEGEOBLOCKING_INVOICEONLY'             => 'Land gehört nicht zum Liefergebiet',
     'OEGEOBLOCKING_INVOICEONLY_HELP'        => 'Bestellungen aus diesem Land sind möglich, werden aber nicht dorthin geliefert. Kunden müssen spätestens im Bestellprozess eine Lieferadresse in einem Land angeben, das beliefert wird.',
     'OEGEOBLOCKING_ADMIN_PICKUP_ADDRESS'     => 'Abholadresse',
 );
--- Application/views/admin/en/oegeoblocking_lang.php
+++ Application/views/admin/en/oegeoblocking_lang.php
@@ -5,7 +5,7 @@ $sLangName = 'English';
 $aLang = array(
     'charset'                 => 'UTF-8',
 
-    'OEGEOBLOCKING_INVOICEONLY'             => 'We\'ll not deliver to this country"',
+    'OEGEOBLOCKING_INVOICEONLY'             => 'We\'ll not deliver to this country',
     'OEGEOBLOCKING_INVOICEONLY_HELP'        => 'Orders are possible, but will not be delivered to this country. Customers must specify a delivery address in a country that we deliver to at the latest during checkout.',
     'OEGEOBLOCKING_ADMIN_PICKUP_ADDRESS'     => 'Pickup address',
 );
