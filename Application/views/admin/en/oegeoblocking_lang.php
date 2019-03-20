0aed7a6413eb4de6ab499b1ec16083c636609766
Benjamin Jörger <benjamin.joerger@oxid-esales.com>
2019-03-20 11:11:55 +0100

    text adjustment

--- Application/views/admin/en/oegeoblocking_lang.php
+++ Application/views/admin/en/oegeoblocking_lang.php
@@ -6,6 +6,6 @@ $aLang = array(
     'charset'                 => 'UTF-8',
 
     'OEGEOBLOCKING_INVOICEONLY'             => 'We\'ll not deliver to this country',
-    'OEGEOBLOCKING_INVOICEONLY_HELP'        => 'Orders are possible, but will not be delivered to this country. Customers must specify a delivery address in a country that we deliver to at the latest during checkout.',
+    'OEGEOBLOCKING_INVOICEONLY_HELP'        => 'Orders from this country are possible, but cannot be delivered. Customers must specify a delivery address in a country that we deliver to at the latest during checkout.',
     'OEGEOBLOCKING_ADMIN_PICKUP_ADDRESS'     => 'Pickup address',
 );
